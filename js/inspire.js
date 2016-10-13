87bb2d866af5e3a169ea31aff8b68764f1d2b7dc
andrew <andrew_rich@hotmail.com>
2016-10-13 08:14:26 -0400

    .each append

--- js/inspire.js
+++ js/inspire.js
@@ -14,22 +14,33 @@ var searchTerm = $("#query").val();
 			var url = "http://api.stackexchange.com/2.2/tags/" + searchTerm + "/top-answerers/all_time?site=stackoverflow";
 
 		$.getJSON(url, params, function(data){
-		//console.log(data.items);
 			showResults(data.items);
+			//console.log(data.items);
 		});
 	}
 
 	function showResults(results){
+
+
 		$.each(results, function(index, value){
-			var name = value.user.display_name;
-			var rep = value.user.reputation;
-			console.log(name);
-		});
-		
+
+			$(".name").append(value.user.display_name);
+			$(".rep").append(value.user.reputation);
+			$(".score").append(value.score);
+			
+			var temp = $('.templates .answerers').clone();
+			
+			var name = temp.find(".name");
+			name.text(value.user.display_name);
+
+		});	
+
+
+
+			$(".hidden").show();
 	}
 
-
-
+	
 });
 	
 
