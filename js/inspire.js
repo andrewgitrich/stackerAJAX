781654c1c7e5e67a4954df1f42ca7b3413e84538
andrew <andrew_rich@hotmail.com>
2016-10-14 10:46:39 -0400

    IT WORKS!

--- js/inspire.js
+++ js/inspire.js
@@ -1,46 +1,51 @@
 $(document).ready(function(){
 
 	$(".inspiration-getter").submit(function(event){
-event.preventDefault();
-var searchTerm = $("#query").val();
+	event.preventDefault();
+	$(".results").html("");
+
+	var searchTerm = $("#query").val();
 	getRequest(searchTerm);
 });
+
 
 	function getRequest(searchTerm){
 		var params = {
 			'tag': searchTerm,
 			};
-		
+
 			var url = "http://api.stackexchange.com/2.2/tags/" + searchTerm + "/top-answerers/all_time?site=stackoverflow";
 
-		$.getJSON(url, params, function(data){
-			showResults(data.items);
-			//console.log(data.items);
-		});
-	}
+			$.getJSON(url, params, function(data){
+			})
 
-	function showResults(results){
+		.done(function(data){
+			 $('.search-results').html(data.items.length + ' results for <strong>' + params.tag + '</strong>');
 
-
-		$.each(results, function(index, value){
-
-			$(".name").append(value.user.display_name);
-			$(".rep").append(value.user.reputation);
-			$(".score").append(value.score);
-			
-			var temp = $('.templates .answerers').clone();
-			
-			var name = temp.find(".name");
-			name.text(value.user.display_name);
-
+			  for (var i = 0; i < data.items.length; i++) {
+                var question = showResults(data.items[i]);
+                $('.results').append(question);
+           		 }
 		});	
-
-
-
-			$(".hidden").show();
 	}
 
-	
+	var showResults = function(question) {
+
+	    var result = $('.templates .answerers').clone();
+
+	    var name = result.find('.name a');
+	    name.attr('href', question.user.link);
+	    name.text(question.user.display_name);
+	    console.log(question.user.display_name);
+
+	    var reputation = result.find('.rep');
+	    reputation.text(question.user.reputation);
+
+	    var score = result.find('.score');
+	    score.text(question.score);
+
+	    return result;
+	};
 });
 	
 
